ccc14497ac7bcd6fdedacda1489834bbe374f5f9
Will <bgigs37@gmail.com>
2020-11-21 23:17:28 -0500

    anota one

--- packages/my-proj/src/hello-world.ts
+++ packages/my-proj/src/hello-world.ts
@@ -5,3 +5,7 @@ export function sayHello() {
 export function sayGoodbye() {
     console.log('goodbye');
 }
+
+
+sayHello();
+sayGoodbye();
---
+++ packages/my-proj/tests/helloworld.spec.ts
@@ -0,0 +1,7 @@
+
+describe('calculate', function() {
+    it('add', function() {
+      let result = 1+2;
+      expect(result).toBe(3);
+    });
+});
